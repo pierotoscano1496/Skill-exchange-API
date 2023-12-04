f4b0d343ae07b4df8038a918a74af6c06e1ab04b
Piero Toscano <iepvptr@gmail.com>
2023-12-04 14:58:29 -0500

    Instanciamiento del test del repositorio del UsuarioRepositoryTester

--- src/test/java/com/main/skillexchangeapi/repositories/UsuarioRepositoryTester.java
+++ src/test/java/com/main/skillexchangeapi/repositories/UsuarioRepositoryTester.java
@@ -4,11 +4,35 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
+@Testcontainers
 public class UsuarioRepositoryTester extends MySQLContainer<UsuarioRepositoryTester> {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Container
+    private static final MySQLContainer mySqlContainer = new MySQLContainer<>("mysql:8.0.33")
+            .withDatabaseName("skill_exchange_dev")
+            .withUsername("skilleduser")
+            .withPassword("piero1496");
+    int port = mySqlContainer.getMappedPort(3306);
+
+    String jdbcUrl = mySqlContainer.getJdbcUrl();
+
+    @DynamicPropertySource
+    private static void setupProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", mySqlContainer::getJdbcUrl);
+        registry.add("spring.datasource.username", mySqlContainer::getUsername);
+        registry.add("spring.datasource.password", mySqlContainer::getPassword);
+    }
 
     @Test
     void testMySQLContainerIsRunning() {
