5765dd1306f847b460659dffd85dd92d7d8c2e48
Piero Toscano <iepvptr@gmail.com>
2024-04-02 16:50:24 -0500

    simple changes-Sec config

--- src/main/java/com/main/skillexchangeapi/apirest/controllers/SimpleCheckController.java
+++ src/main/java/com/main/skillexchangeapi/apirest/controllers/SimpleCheckController.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "simple-check", produces = "application/json")
-@CrossOrigin
 public class SimpleCheckController {
 
     @GetMapping
