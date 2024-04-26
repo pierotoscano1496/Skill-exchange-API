b35d54f3888b8169be34ddcd1fe575bf9436c2a6
Piero Toscano <iepvptr@gmail.com>
2024-04-26 09:03:28 -0500

    CorsMapping y permisión de endpoint de conexión con broker

---
+++ src/main/java/com/main/skillexchangeapi/app/security/WebConfig.java
@@ -0,0 +1,21 @@
+package com.main.skillexchangeapi.app.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowCredentials(false)
+                .maxAge(3600)
+                .allowedHeaders("Accept", "Content-Type", "Origin", "Authorization", "X-Auth-Token")
+                .exposedHeaders("X-Auth-Token", "Authorization")
+                .allowedMethods("POST", "GET", "DELETE", "PUT", "OPTIONS");
+    }
+}
