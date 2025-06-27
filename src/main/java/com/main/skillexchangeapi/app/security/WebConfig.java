10b826f14d8725bba2940dc635563c8915c1919d
Piero <iepvptr@gmail.com>
2025-06-27 08:06:28 -0500

    Cors por perfil

--- src/main/java/com/main/skillexchangeapi/app/security/WebConfig.java
+++ src/main/java/com/main/skillexchangeapi/app/security/WebConfig.java
@@ -1,6 +1,8 @@
 package com.main.skillexchangeapi.app.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -8,10 +10,30 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 @EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
+    @Autowired
+    private Environment environment;
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
+        String[] activeProfiles = environment.getActiveProfiles();
+        boolean isProd = false;
+
+        for (String profile : activeProfiles) {
+            if ("prod".equals(profile)) {
+                isProd = true;
+                break;
+            }
+        }
+
+        String[] allowedOrigins;
+        if (isProd) {
+            allowedOrigins = new String[] { "http://tuchambita.com/" };
+        } else {
+            allowedOrigins = new String[] { "http://localhost:3000" };
+        }
+
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000", "https://skill-exchange-frontend-2fc468262f15.herokuapp.com")
+                .allowedOrigins(allowedOrigins)
                 .allowCredentials(false)
                 .maxAge(3600)
                 .allowedHeaders("Accept", "Content-Type", "Origin", "Authorization", "X-Auth-Token")
